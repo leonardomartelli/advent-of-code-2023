1655990f80074c575fe549f838f61bb310c547ea
leonardomartelli <leonardo@martellioliveira.me>
2023-12-01 12:38:45 -0300

    finishing day 1, puzzle 2

--- day-1/day-1-2.py
+++ day-1/day-1-2.py
@@ -1,26 +1,16 @@
 import re
 
-pattern = r'one|two|three|four|five|six|seven|eight|nine|\d'
-
 numbers = {
-    'one': 1,
-    'two': 2,
-    'three': 3,
-    'four': 4,
-    'five': 5,
-    'six': 6,
-    'seven': 7,
-    'eight': 8,
-    'nine': 9,
+    'one': 'o1e',
+    'two': 't2o',
+    'three': 't3e',
+    'four': 'f4r',
+    'five': 'f5e',
+    'six': 's6x',
+    'seven': 's7n',
+    'eight': 'e8t',
+    'nine': 'n9e',
 }
-
-
-def parse(string):
-    if string.isnumeric():
-        return int(string)
-
-    return numbers[string]
-
 
 puzzle_input_file = open('input.txt', 'r')
 puzzle_input = puzzle_input_file.read()
@@ -32,17 +22,20 @@ sum = 0
 for calibration in calibrations:
     first, last = -1, -1
 
-    caught = re.findall(pattern, calibration)
+    for key in numbers:
+        calibration = calibration.replace(key, numbers[key])
+
+    caught = re.findall('\d', calibration)
 
     if len(caught) == 1:
-        first = last = parse(caught[0])
+        first = last = int(caught[0])
     else:
-        first = parse(caught[0])
-        last = parse(caught[len(caught) - 1])
+        first = int(caught[0])
+        last = int(caught[len(caught) - 1])
 
     num = first*10 + last
 
-    print(num)
+    print(calibration, caught, num)
     sum += num
 
 print(sum)
