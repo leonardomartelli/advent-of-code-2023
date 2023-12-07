11da608e96ae2ac9fcedfe95315fb912b4ea621f
leonardomartelli <leonardo@martellioliveira.me>
2023-12-07 19:30:26 -0300

    day 6

---
+++ day-06/day-6-1.py
@@ -0,0 +1,24 @@
+puzzle_input_file = open('input.txt', 'r')
+puzzle_input = puzzle_input_file.read()
+
+inputs = puzzle_input.split('\n')
+durations = [int(d) for d in inputs[0].split(':')[1:]]
+records = [int(r) for r in inputs[1].split(':')[1:]]
+
+possible_ways = 1
+
+for i in range(len(durations)):
+    race = durations[i]
+    record = records[i]
+
+    ways_to_beat = 0
+
+    for possibilty in range(race):
+        performance = (race - possibilty) * possibilty
+        if performance > record:
+            ways_to_beat += 1
+
+    if ways_to_beat > 0:
+        possible_ways *= ways_to_beat
+
+print(possible_ways)
